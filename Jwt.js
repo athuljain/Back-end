29207dab531dc215bed531adeeee8addfa9de92c
athuljain <athuljainkj2@gmail.com>
2024-06-12 15:07:58 +0530

    jwt

---
+++ Jwt.js
@@ -0,0 +1,114 @@
+// const express = require('express');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const User = require('./JwtUserModel');
+// const router = express.Router();
+
+// // const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+// // User registration
+// router.post('/register', async (req, res) => {
+//     try {
+//         const user = await User.findOne({ email: req.body.email });
+
+//         if (user) {
+//             return res.status(409).json({ error: "Email already exists." });
+//         }
+
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+//         const newUser = await User.create({
+//             name: req.body.name,
+//             // location: req.body.location,
+//             email: req.body.email,
+//             password: hashedPassword
+//         });
+
+//         res.status(201).json({ success: true, newUser });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ success: false, msg: "An error occurred" });
+//     }
+// });
+
+// // User login
+// router.post('/login', async (req, res) => {
+//     try {
+//         const user = await User.findOne({ email: req.body.email });
+
+//         if (user) {
+//             const comparePwd = await bcrypt.compare(req.body.password, user.password);
+
+//             if (comparePwd) {
+//                 const authToken = jwt.sign({ email: user.email }, jwtSecretKey, { expiresIn: '1d' });
+//                 res.json({ success: true, authToken, user, userId: user._id });
+//             } else {
+//                 res.status(400).json({ error: 'Incorrect password!', success: false });
+//             }
+//         } else {
+//             res.status(404).json({ error: 'User not found', success: false });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ success: false, message: 'An error occurred' });
+//     }
+// });
+
+// module.exports = router;
+
+
+
+
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('./JwtUserModel');
+const router = express.Router();
+
+// User registration
+router.post('/register', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            return res.status(409).json({ error: "Email already exists." });
+        }
+
+         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({ success: true, newUser });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, msg: "An error occurred" });
+    }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            const comparePwd = await bcrypt.compare(req.body.password, user.password);
+
+            if (comparePwd) {
+                res.json({ success: true, user, userId: user._id });
+            } else {
+                res.status(400).json({ error: 'Incorrect password!', success: false });
+            }
+        } else {
+            res.status(404).json({ error: 'User not found', success: false });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'An error occurred' });
+    }
+});
+
+module.exports = router;
+
---
+++ JwtServer.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/user-auth-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.log('Failed to connect to MongoDB', err);
+});
+
+// Routes
+const router = require('./Jwt');
+app.use('/users', router);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
