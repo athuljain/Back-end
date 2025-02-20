8ea16fb6e348d7326ba689485f4faac4c9644a58
athuljain <athuljainkj2@gmail.com>
2025-02-20 09:44:33 +0530

    nodeup

--- BFile.js
+++ BFile.js
@@ -1,44 +1,45 @@
-const fs= require('fs')
+const fileSystem= require('fs')
 
 //write file
 
-// fs.writeFile('./example3.txt', 'Hi, sdckjsdc bzicbsd jbsckjwe', (err) => {
-//     if (err) throw err;
-//     console.log('File has been written.');
-//   });
+fileSystem.writeFile('./fileSystem.txt', 'Hi, file system ', (err) => {
+    if (err) throw err;
+    console.log('File has been written.');
+  });
 
 
 //  ReadFile
 
-// fs.readFile('example2.txt', 'utf-8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//   });
+fileSystem.readFile('example3.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
 
 // //append File
 
-// fs.appendFile('example2.txt', ' good mrng. from soften', 'utf8', (err) => {
-//     if (err) throw err;
-//     console.log('Data has been appended to the file.');
-//   });
+fileSystem.appendFile('fileSystem.txt', ' good mrng. from soften', 'utf8', (err) => {
+    if (err) throw err;
+    console.log('Data has been appended to the file.');
+  });
 
 
 // // // // Delete file
-// fs.unlink('example2.txt', (err) => {
-//     if (err) throw err;
-//     console.log('File has been deleted.');
-//   });
+
+fileSystem.unlink('example3.txt', (err) => {
+    if (err) throw err;
+    console.log('File has been deleted.');
+  });
 
 
 
 
    
-    // fs.writeFile('example1.txt', 'This is new content.', 'utf8', (err) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     console.log('File content has been replaced with new data.');
-    // });
+    fs.writeFile('example1.txt', 'This is new content.', 'utf8', (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('File content has been replaced with new data.');
+    });
 
 
 
@@ -60,30 +61,30 @@ const fs= require('fs')
 
 
 
-//    // node routes
+   // node routes
 
-    // const http = require('http');
-    // const port = 5000;
+    const http = require('http');
+    const port = 5000;
     
-    // const server = http.createServer((req, res) => {
-    //     if (req.url === '/') {
-    //         res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //         res.end('Hello, this is the home page!');
-    //     } else if (req.url === '/about') {
-    //         res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //         res.end('Welcome to the about page!');
-    //     } else if (req.url === '/contact') {
-    //         res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //         res.end('Contact us at contact@example.com');
-    //     } else {
-    //         res.writeHead(404, { 'Content-Type': 'text/plain' });
-    //         res.end('Page not found');
-    //     }
-    // });
+    const server = http.createServer((req, res) => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello, this is the home page!');
+        } else if (req.url === '/about') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Welcome to the about page!');
+        } else if (req.url === '/contact') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Contact us at contact@example.com');
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Page not found');
+        }
+    });
     
-    // server.listen(port, () => {
-    //     console.log(`Server listening on port ${port}`);
-    // });
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
     
 
 
