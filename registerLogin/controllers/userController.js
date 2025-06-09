31603fd2988199808dcc0be0907e9f04efbdf2f5
athuljain <athuljainkj2@gmail.com>
2025-06-09 08:29:02 +0530

    registerLogin update

--- registerLogin/controllers/userController.js
+++ registerLogin/controllers/userController.js
@@ -22,3 +22,27 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+
+
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(404).json({ message: 'User not found' });
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: 'Invalid credentials' });
+
+    res.status(200).json({ message: 'Login successful', user: { name: user.name, email: user.email } });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
