9a44ea26a33bdbcbe57f9bcde104602588ef2ed9
athuljain <athuljainkj2@gmail.com>
2025-10-23 08:50:44 +0530

    js update

--- BFile.js
+++ BFile.js
@@ -1,8 +1,8 @@
-// const fileSystem= require('fs')
+const fileSystem= require('fs')
 
-// // write file
+// write file
 
-// fileSystem.writeFile('./fileSystems.txt', 'soften mern full stack  ', (err) => {
+// fileSystem.writeFile('./fileSystem.txt', 'fs module write file   ', (err) => {
 //     if (err) throw err;
 //     console.log('File has been written.');
 //   });
@@ -22,6 +22,8 @@
 //     console.log('Data has been appended to the file.');
 //   });
 
+
+
 //   fileSystem.writeFile('./fileSystem.txt',"node js from soften tech",(err)=>{
 //     if(err) throw err;
 //     console.log("write file");
@@ -32,7 +34,7 @@
 
 // // // // Delete file
 
-// fileSystem.unlink('./fileSystems.txt', (err) => {
+// fileSystem.unlink('./fileSystem.txt', (err) => {
 //     if (err) throw err;
 //     console.log('File has been deleted.');
 //   });
@@ -93,36 +95,36 @@
 
 
 
-    // const http = require('http');
-    // const url = require('url');
-    // const port = 5000;
+    const http = require('http');
+    const url = require('url');
+    const port = 5000;
     
-    // const server = http.createServer((req, res) => {
-    // // Parse the request URL
-    // const parsedUrl = url.parse(req.url, true);
+    const server = http.createServer((req, res) => {
+    // Parse the request URL
+    const parsedUrl = url.parse(req.url, true);
     
-    // // Log the requested URL path
-    // console.log(`Received ${req.method} request for ${parsedUrl.pathname}`);
+    // Log the requested URL path
+    console.log(`Received ${req.method} request for ${parsedUrl.pathname}`);
     
-    // // Route the request based on the path
-    // if (parsedUrl.pathname === '/') {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    // res.end('Hello, this is the home page!');
-    // } else if (parsedUrl.pathname === '/about') {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    // res.end('Welcome to the about page!');
-    // } else if (parsedUrl.pathname === '/contact') {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    // res.end('Contact us at contact@example.com');
-    // } else {
-    // // Handle unknown paths with a 404 response
-    // res.writeHead(404, { 'Content-Type': 'text/plain' });
-    // res.end('Page not found');
-    // }
-    // });
+    // Route the request based on the path
+    if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, this is the home page!');
+    } else if (parsedUrl.pathname === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the about page!');
+    } else if (parsedUrl.pathname === '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Contact us at contact@example.com');
+    } else {
+    // Handle unknown paths with a 404 response
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+    }
+    });
     
-    // // Start the server
-    // server.listen(port, () => {
-    // console.log(`Server listening on port ${port}`);
-    // });
+    // Start the server
+    server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    });
     
