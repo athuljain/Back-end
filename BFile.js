e1ad408538e3798929adc8fc0b5dd892e78c7b62
athuljain <athuljainkj2@gmail.com>
2025-11-06 14:37:23 +0530

    node js updates

--- BFile.js
+++ BFile.js
@@ -34,10 +34,10 @@ const fileSystem= require('fs')
 
 // // // // Delete file
 
-// fileSystem.unlink('./fileSystem.txt', (err) => {
-//     if (err) throw err;
-//     console.log('File has been deleted.');
-//   });
+fileSystem.unlink('./fileSystem.txt', (err) => {
+    if (err) throw err;
+    console.log('File has been deleted.');
+  });
 
 
 
@@ -95,36 +95,36 @@ const fileSystem= require('fs')
 
 
 
-    const http = require('http');
-    const url = require('url');
-    const port = 5000;
+    // const http = require('http');
+    // const url = require('url');
+    // const port = 5000;
     
-    const server = http.createServer((req, res) => {
-    // Parse the request URL
-    const parsedUrl = url.parse(req.url, true);
+    // const server = http.createServer((req, res) => {
+    // // Parse the request URL
+    // const parsedUrl = url.parse(req.url, true);
     
-    // Log the requested URL path
-    console.log(`Received ${req.method} request for ${parsedUrl.pathname}`);
+    // // Log the requested URL path
+    // console.log(`Received ${req.method} request for ${parsedUrl.pathname}`);
     
-    // Route the request based on the path
-    if (parsedUrl.pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, this is the home page!');
-    } else if (parsedUrl.pathname === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome to the about page!');
-    } else if (parsedUrl.pathname === '/contact') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Contact us at contact@example.com');
-    } else {
-    // Handle unknown paths with a 404 response
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Page not found');
-    }
-    });
+    // // Route the request based on the path
+    // if (parsedUrl.pathname === '/') {
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('Hello, this is the home page!');
+    // } else if (parsedUrl.pathname === '/about') {
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('Welcome to the about page!');
+    // } else if (parsedUrl.pathname === '/contact') {
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('Contact us at contact@example.com');
+    // } else {
+    // // Handle unknown paths with a 404 response
+    // res.writeHead(404, { 'Content-Type': 'text/plain' });
+    // res.end('Page not found');
+    // }
+    // });
     
-    // Start the server
-    server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-    });
+    // // Start the server
+    // server.listen(port, () => {
+    // console.log(`Server listening on port ${port}`);
+    // });
     
